79b91b91bf11633399457b383a81c15c8104e5ca
nomad-mystic <nomadmystics@gmail.com>
2016-05-26 11:45:41 -0700

    Created four function checkScreenSize(), getMobileLocation(), MobileOriginLocation(), and mobileLocationError() for collecting navigator.location from mobile users with a screen size of <=640px

--- includes/js/scripts.js
+++ includes/js/scripts.js
@@ -4,8 +4,49 @@
 
 
 $(function() {
-    
-    var windowWidth = $(window).width();
-    
-    
+
+    // check to see if the user is on a mobile device
+    function checkScreenSize() {
+        var windowWidth = $(window).width();
+        console.log(windowWidth);
+
+        if (windowWidth <= 640) {
+            console.log('small screen');
+            getMobileLocation();
+        } // end if
+    } // end checkScreenSize
+    checkScreenSize();
+
+    // if user is on a mobile device get location of user
+    function getMobileLocation() {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(mobileOriginLocation, mobileLocationError);
+        }
+    } // end getLocation
+
+    // if the getLocation is successful
+    function mobileOriginLocation(location) {
+        // location is the object returned from the getCurrentPosition() function
+        console.log('latitude ' + location.coords.latitude);
+        console.log('longitude ' + location.coords.longitude);
+    } // end originLocation()
+
+    // errors switch if unsuccessful
+    function mobileLocationError(error) {
+        switch(error.code) {
+            case error.PERMISSION_DENIED:
+                // x.innerHTML = "User denied the request for Geolocation.";
+                break;
+            case error.POSITION_UNAVAILABLE:
+                // x.innerHTML = "Location information is unavailable.";
+                break;
+            case error.TIMEOUT:
+                // x.innerHTML = "The request to get user location timed out.";
+                break;
+            case error.UNKNOWN_ERROR:
+                // x.innerHTML = "An unknown error occurred.";
+                break;
+        } // end switch
+    } // end locationError()
+
 }); // end ready
